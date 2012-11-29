cc20e2a8575bff1c038cb0e6ed08644bf13b8990
Brad Chen <brad5620732@hotmail.com>
2012-11-29 13:33:08 -0500

    Add logger.

--- src/main/java/com/bc/sass/faces/SassResourceHandler.java
+++ src/main/java/com/bc/sass/faces/SassResourceHandler.java
@@ -4,6 +4,9 @@ import javax.faces.application.Resource;
 import javax.faces.application.ResourceHandler;
 import javax.faces.application.ResourceHandlerWrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bc.sass.Syntax;
 
 /**
@@ -15,6 +18,8 @@ public class SassResourceHandler extends ResourceHandlerWrapper {
 
 	private static final String SASS_EXTENSION = ".sass";
 	private static final String SCSS_EXTENSION = ".scss";
+	private static final Logger LOGGER
+			= LoggerFactory.getLogger(SassResourceHandler.class);
 
 	private final ResourceHandler wrapped;
 
@@ -31,8 +36,10 @@ public class SassResourceHandler extends ResourceHandlerWrapper {
 	public Resource createResource(String resourceName, String libraryName) {
 		Resource resource = wrapped.createResource(resourceName, libraryName);
 		if (resourceName.endsWith(SASS_EXTENSION)) {
+			LOGGER.debug("Compile SASS file {}...", resourceName);
 			return new SassResource(resource, Syntax.SASS);
 		} else if (resourceName.endsWith(SCSS_EXTENSION)) {
+			LOGGER.debug("Compile SCSS file {}...", resourceName);
 			return new SassResource(resource, Syntax.SCSS);
 		}
 		return resource;
